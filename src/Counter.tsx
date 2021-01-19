b05b457be9b66ccaf67f66c8d19c407ab993968d
Andrey Konstantinov <avkonst@gmail.com>
2021-01-19 14:02:48 +1300

    reproducer for https://github.com/facebook/react/issues/20609

---
+++ src/Counter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "./hookstate-simplified";
+
+function Counter() {
+	const counter = useState(0);
+	const test = useState(0);
+
+	return (
+		<div>
+			<h1>Open React Dev Tools Components panel, click on Counter component and observe the crash in the logging console.</h1>
+		</div>
+	);
+}
+
+export default Counter;
