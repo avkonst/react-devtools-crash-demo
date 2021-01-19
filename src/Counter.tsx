ba54f9578220670299b32ccfd5715e53c41b7854
Andrey Konstantinov <avkonst@gmail.com>
2021-01-19 15:16:37 +1300

    minor change

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 function useState() {
     React.useState(0);
-    React.useEffect(() => {
-        return () => {}
-    });
+    React.useEffect(() => () => {});
 }
 
 function Counter() {
 	useState();
 	React.useState(0);
-	return (
-		<div>Open React Dev Tools Components panel, click on Counter component and observe the crash in the logging console.</div>
-	);
+	return <div>Open React Dev Tools Components panel,
+		click on Counter component and
+		observe the crash in the logging console.</div>;
 }
 
 export default Counter;
